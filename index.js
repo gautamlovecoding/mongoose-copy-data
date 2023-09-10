d559398850de1fe2d56bc55d14d8af96425b8736
gautam kumar <gautam.kumar@dhwaniris.com>
2023-09-10 13:13:25 +0530

    version added.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import { SingleBar } from 'cli-progress';
 import figlet from 'figlet';
 
 program
-  .version('2.2.0')
+  .version('2.3.0')
   .description('Database Copy Utility')
   .option('-s, --source <source>', 'Source database connection string')
   .option('-t, --target <target>', 'Target database connection string')
